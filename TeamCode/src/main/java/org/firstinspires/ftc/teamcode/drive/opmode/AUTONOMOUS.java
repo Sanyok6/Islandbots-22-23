0e945277eac043b960c0b8fad672ce2424eafc78
Sanyok6 <tishchenkoa2008@gmail.com>
2023-01-02 10:21:12 -0500

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AUTONOMOUS.java
@@ -0,0 +1,144 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import android.graphics.Color;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.drive.ComputerVision;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+import org.firstinspires.ftc.teamcode.drive.ComputerVision.SignalSleevePipeline.Colors;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Config
+@Autonomous(group = "drive")
+public class AUTONOMOUS extends LinearOpMode {
+
+    public static double FORWARD_DIST = 45;
+    public static double STRAFE_DIST = 60;
+
+    public volatile Colors color;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        DcMotor lights = hardwareMap.dcMotor.get("Lights");
+        lights.setPower(1);
+
+        DistanceSensor RightDist = hardwareMap.get(DistanceSensor.class, "RightDist");
+        DistanceSensor LeftDist = hardwareMap.get(DistanceSensor.class, "LeftDist");
+
+        ComputerVision vision = new ComputerVision(hardwareMap, telemetry);
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        DcMotor LSmotor = hardwareMap.dcMotor.get("LSmotor");
+        Servo clawServo = hardwareMap.servo.get("claw");
+
+        Trajectory trajectoryForward = drive.trajectoryBuilder(new Pose2d())
+                .forward(FORWARD_DIST)
+                .build();
+
+        Trajectory trajectoryBackward = drive.trajectoryBuilder(new Pose2d())
+                .back(FORWARD_DIST)
+                .build();
+
+        Trajectory strafeRight = drive.trajectoryBuilder(new Pose2d())
+                .strafeRight(STRAFE_DIST)
+                .build();
+
+        Trajectory strafeLeft = drive.trajectoryBuilder(new Pose2d())
+                .strafeLeft(STRAFE_DIST)
+                .build();
+
+//        TrajectorySequence traj = drive.trajectorySequenceBuilder(new Pose2d(-35, -61, Math.toRadians(90)))
+//                .splineToConstantHeading(new Vector2d(-34, -40), Math.toRadians(90))
+//                .splineToConstantHeading(new Vector2d(0, -30), Math.toRadians(90))
+//                .build();
+
+        waitForStart();
+
+        sleep(500);
+
+
+        sleep(2000);
+
+        color = vision.pipeline.color;
+
+        clawServo.setPosition(0.7);
+
+        drive.followTrajectory(trajectoryForward);
+        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).strafeRight(STRAFE_DIST*1.5).build());
+
+        sleep(1000);
+
+        LSmotor.setTargetPosition(3100);
+        LSmotor.setPower(0.9);
+
+        sleep(1500);
+
+        LSmotor.setPower(0.1);
+
+
+        double r = RightDist.getDistance(DistanceUnit.CM);
+        double l = LeftDist.getDistance(DistanceUnit.CM);
+
+        while (!(r > l-10 && r < l+10)) {
+            r = RightDist.getDistance(DistanceUnit.CM);
+            l = LeftDist.getDistance(DistanceUnit.CM);
+
+            if (r < l) {
+                drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).strafeLeft(l > 100 ? 10 : l - r).build());
+            } else {
+                drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).strafeRight(r > 100 ? 10 : r - l).build());
+            }
+        }
+
+        telemetry.addData("RightDist", RightDist.getDistance(DistanceUnit.CM));
+        telemetry.addData("LeftDist", LeftDist.getDistance(DistanceUnit.CM));
+        telemetry.addData("d", Math.sqrt(r*r-225));
+        telemetry.update();
+
+        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).forward((Math.sqrt(Math.pow((r+l)/2, 2)-225))-22).build());
+
+        clawServo.setPosition(0.3);
+        sleep(300);
+        clawServo.setPosition(0.7);
+
+        drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).back(10).build());
+
+        LSmotor.setPower(-0.7);
+
+        sleep(1500);
+
+        LSmotor.setPower(0);
+
+        if (color == Colors.BLUE) {
+            drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).strafeLeft(STRAFE_DIST*2.5).build());
+        } else if (color == Colors.RED) {
+            drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).strafeLeft(STRAFE_DIST*1.5).build());
+        } else {
+            drive.followTrajectory(drive.trajectoryBuilder(new Pose2d()).strafeLeft(STRAFE_DIST*0.5).build());
+        }
+
+
+        while (!isStopRequested() && opModeIsActive()) {
+            telemetry.addData("RightDist", RightDist.getDistance(DistanceUnit.CM));
+            telemetry.addData("LeftDist", LeftDist.getDistance(DistanceUnit.CM));
+            telemetry.update();
+        };
+
+    }
+}
